1d7b96aa67fedd2541a00a6717643f7b09d439d4
samvillasmith <samuel.villa.smith@hotmail.com>
2021-07-07 19:03:43 -0500

    Final Commit

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import { View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
+import { withNavigation } from 'react-navigation';
 import ResultsDetail from './ResultsDetail';
 
 const ResultsList =({ title, results, navigation })=> {
+
+    if(!results.length){
+        return null;
+    }
+
     return(
         <View style={styles.container}>
         <Text style={styles.title}>{title}</Text>
@@ -14,7 +20,7 @@ const ResultsList =({ title, results, navigation })=> {
             renderItem={({item})=>{
                 return (
                 <TouchableOpacity 
-                    onPress={()=> navigation.navigate("ResultsShow")}
+                    onPress={()=> navigation.navigate("ResultsShow", { id:item.id })}
                 >
                     <ResultsDetail result={item}/>
                 </TouchableOpacity>
@@ -39,4 +45,4 @@ const styles = StyleSheet.create({
 
 });
 
-export default ResultsList;
+export default withNavigation(ResultsList);
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -4,7 +4,7 @@ import SearchBar from '../components/SearchBar';
 import useResults from '../hooks/useResults';
 import ResultsList from '../components/ResultsList';
 
-const SearchScreen =({ navigation })=>{
+const SearchScreen =()=>{
     const [term, setTerm] = useState('');
     const [searchApi, results, errorMessage] = useResults();
     
@@ -27,23 +27,23 @@ const SearchScreen =({ navigation })=>{
                 <ResultsList 
                 results={filterResultsByPrice("$")}
                 title="Cost Effective"
-                navigation={navigation}
+              
                 />
 
                 <ResultsList 
                 results={filterResultsByPrice("$$")} 
                 title="Expensive"
-                navigation={navigation}
+                
                 />
 
                 <ResultsList results={filterResultsByPrice("$$$")} 
                 title="Special Occasion"
-                navigation={navigation}
+            
                 />
 
                 <ResultsList results={filterResultsByPrice("$$$$")} 
                 title="Very Expensive"
-                navigation={navigation}
+               
                 />
 
             </ScrollView>
